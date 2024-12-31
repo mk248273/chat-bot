7666d79e629e259a88135b590f890c601aae09bd
muzammil7ctech <mkn144248273@gmail.com>
2024-12-31 19:14:56 +0500

    prompt updated

--- app.py
+++ app.py
@@ -46,10 +46,12 @@ def get_vector_store(chunks):
 
 def get_conversational_chain():
     prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    Context:\n {context}?\n
-    Question: \n{question}\n
+    instructions:
+        Answer the question from the provided context,
+        if the answer is not in provided context just say, "answer is not available in the context"
+        don't provide the wrong answer\n\n
+    Context:\n {context}\n
+    Question: \n{question}?\n
 
     Answer:
     """
